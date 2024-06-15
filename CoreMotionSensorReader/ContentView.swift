69acda0f745d784c3b30ee5af71bb85a26b6bcdd
Maurice Richter <maurice.ri01@gmail.com>
2024-06-15 17:59:54 +0200

    Initial Commit

---
+++ CoreMotionSensorReader/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CoreMotionSensorReader
+//
+//  Created by Maurice Richter on 15.06.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ CoreMotionSensorReader/CoreMotionSensorReaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoreMotionSensorReaderApp.swift
+//  CoreMotionSensorReader
+//
+//  Created by Maurice Richter on 15.06.24.
+//
+
+import SwiftUI
+
+@main
+struct CoreMotionSensorReaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
